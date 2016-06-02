eb331bd13e53d2636eaacc4a75868a593275bc1f
zhangshiping <shiping.zhang@1905.com>
2016-06-02 08:55:37 +0800

    路由修改

---
+++ public/http/admin/apiController.js
@@ -0,0 +1,20 @@
+/**
+ * Created by zhangshiping on 16-5-12.
+ */
+//require.s.contexts._.config.paths.data = '/Admin/index';
+//dump(require.s.contexts._.defined.data);
+
+define(['app',dataPath(),'admin/public/headerController','admin/public/leftController'], function (app,datas) {
+    var datas = datas || data;
+
+    dump(datas);
+    app.register.controller('admin-apiCtrl', ["$scope", '$rootScope','Model','View','$alert', function ($scope,$rootScope,Model,View,$alert) {
+        $scope = View.with(datas,$scope);
+        $rootScope.nav = datas.nav;
+        /* 获取数据 */
+        $scope.getData = Model.getData;
+    }]);
+})
+
+
+
