a3c9c52cf0ecf7c7e5ba167efc78156f4d8ea078
unknown <zsping1989@github.com>
2016-06-11 18:31:19 +0800

    测试

--- app/Providers/ResponseMacroServiceProvider.php
+++ app/Providers/ResponseMacroServiceProvider.php
@@ -19,24 +19,20 @@ class ResponseMacroServiceProvider extends ServiceProvider
      */
     public function boot(ResponseFactory $factory)
     {
-        $factory->macro('returns', function ($value,$status=200) use ($factory) {
-            //添加全局值
+        $macro = $this;
+        $factory->macro('returns', function ($value,$status=200) use ($factory,$macro) {
             $data = [
                 'order'=> Request::input('order',[]), //排序
-                'where'=>Request::input('where',[]), //条件查询
-                'route'=>Request::getPathInfo()  //路由信息
+                'where'=>Request::input('where',[]) //条件查询
             ];
-
-
             $value = collect($value)->merge($data);
-            $menu = Menu::where('url','=',$value->get('route'))->first();
-            $value['nav'] = $menu ? collect($menu->parents(true)->toArray())->keyBy('id') : [];
-
             if(Request::input('callback')){ //jsonp
                return $factory->jsonp(Request::input('callback'),$value);
             }elseif(Request::input('define')=='AMD'){ //AMD
+                $macro->addData($value);
                 $value = 'define([],function(){ return '.collect($value)->toJson().';});';
             }elseif(Request::input('define')=='CMD'){ //CMD
+                $macro->addData($value);
                 $value = 'define([],function(){ return '.collect($value)->toJson().';});';
             }elseif(Request::has('dd')){ //数据打印页面
                 dd($value->toArray());
@@ -45,12 +41,25 @@ class ResponseMacroServiceProvider extends ServiceProvider
             }elseif(Request::has('script')){ //页面
                 $value = 'var data = '.collect($value)->toJson().';';
             }else{
+                $macro->addData($value);
                 $value['user'] = Auth::user(); //用户信息
                 $value['menus'] = session('admin.menus');
                 return view('index',['data'=>$value]);
             }
             return $factory->make($value,$status);
         });
+    }
+    public function addData(&$value){
+        $data = [
+            'order'=> Request::input('order',[]), //排序
+            'where'=>Request::input('where',[]), //条件查询
+            'route'=>Request::getPathInfo()  //路由信息
+        ];
+
+
+        $value = collect($value)->merge($data);
+        $menu = Menu::where('url','=',$value->get('route'))->first();
+        $value['nav'] = $menu ? collect($menu->parents(true)->toArray())->keyBy('id') : [];
     }
 
     /**
