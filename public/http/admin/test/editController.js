127a1f23c26db8ce0d71f36cecb8149f1a9943fe
zhangshiping <shiping.zhang@1905.com>
2016-06-21 17:25:52 +0800

    测试

--- public/http/admin/test/editController.js
+++ public/http/admin/test/editController.js
@@ -1,4 +1,5 @@
 define(['app',dataPath(),'admin/public/headerController','admin/public/leftController'], function (app,datas) {
+    //alert(dataPath());
     var datas = datas || data;
     dump(datas);
     app.register.controller('admin-test-editCtrl', ["$scope",'$rootScope', 'Model','View','$alert','$http','$location','$timeout',
@@ -18,7 +19,7 @@ define(['app',dataPath(),'admin/public/headerController','admin/public/leftContr
             }).success(function(){
                 $timeout(function(){
                     if($scope.row.id){
-                        //$location.path($scope.back_url);
+                        $location.path($scope.back_url);
                     }
                 },1000)
             });
